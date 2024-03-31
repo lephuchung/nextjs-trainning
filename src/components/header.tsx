17dec691eb36763a6be2e50da00550239c04bd3f
lephuchung <lephuchung003@gmail.com>
2024-03-31 22:10:16 +0700

    video23

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,9 +9,9 @@ function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">
-                    <Link href="/" className='navbar-brand'>NextJs</Link>
-                </Navbar.Brand>
+                {/* <Navbar.Brand href=""> */}
+                <Link href="/" className='navbar-brand'>NextJs</Link>
+                {/* </Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
