4dd3789bab2d70fab43761bf4fd7ca5cc83176f4
lephuchung <lephuchung003@gmail.com>
2024-03-31 21:14:57 +0700

    video22

---
+++ src/app/example2/youtube/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Youtube = () => {
+    const router = useRouter()
+
+    const handleOnClickBack = () => {
+        router.push('/example2')
+    }
+    return (
+        <div>
+            <span>Youtube page</span>
+            <div>
+                <Button variant='primary' onClick={() => handleOnClickBack()}>Back to home page</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Youtube
