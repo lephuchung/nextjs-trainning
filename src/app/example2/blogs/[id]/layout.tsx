3a38b7f6f23620ab05dd7dde998b02aed32dc051
lephuchung <lephuchung003@gmail.com>
2024-04-02 02:16:13 +0700

    video30

--- src/app/example2/blogs/[id]/layout.tsx
+++ src/app/example2/blogs/[id]/layout.tsx
@@ -1,22 +1,18 @@
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+
 import { Container } from 'react-bootstrap'
-import MyTable from '@/components/table'
-import { ToastContainer, toast } from 'react-toastify';
+import Link from "next/link";
 import 'react-toastify/dist/ReactToastify.css';
-export default function Ex2Layout({
+export default function BlogDetailLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
         <>
-            <Header />
             <Container style={{ minHeight: 'calc(100vh - 80px)' }}>
+                <Link className='btn btn-primary my-3' href={'/example2/blogs'}>Back to blogs page</Link>
                 {children}
             </Container>
-            <Footer />
-            <ToastContainer />
         </>
 
     )
--- src/app/example2/blogs/[id]/page.tsx
+++ src/app/example2/blogs/[id]/page.tsx
@@ -1,9 +1,62 @@
+'use client'
+import Link from 'next/link';
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import useSWR from 'swr';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 const BlogDetail = ({ params }: { params: { id: string } }) => {
-    console.log('check props: ', params.id);
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+    const { data, error, isLoading } = useSWR(
+        `http://localhost:8000/blogs/${params.id}`,
+        fetcher,
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false
+        }
+    );
+
+    if (!data || isLoading) {
+        return (
+            <Card style={{ width: 'lg' }}>
+                <Card.Header>
+                    <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={6} />
+                    </Placeholder>
+                </Card.Header>
+                <Card.Body>
+                    <Placeholder as={Card.Title} animation="glow">
+                        <Placeholder xs={6} />
+                    </Placeholder>
+                    <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                        <Placeholder xs={6} /> <Placeholder xs={8} />
+                    </Placeholder>
+                </Card.Body>
+            </Card>
+        )
+    }
+    console.log('check props: ', params.id, data);
     return (
-        <div>BlogDetail - {params.id}</div>
+        <>
+            <Card
+                bg={'light'}
+                key={'Light'}
+                text={'light' === 'light' ? 'dark' : 'white'}
+                style={{ width: 'lg' }}
+                className="mb-2"
+            >
+                <Card.Header>{data.author}</Card.Header>
+                <Card.Body>
+                    <Card.Title> {data.title} </Card.Title>
+                    <Card.Text>
+                        {data.content}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
