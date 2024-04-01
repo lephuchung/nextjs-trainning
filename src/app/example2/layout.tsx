d97dabd2c42b4e2c254d570de3f69751c4f6a4ad
lephuchung <lephuchung003@gmail.com>
2024-04-01 23:34:12 +0700

    video26

--- src/app/example2/layout.tsx
+++ src/app/example2/layout.tsx
@@ -2,6 +2,8 @@ import Header from '@/components/header'
 import Footer from '@/components/footer'
 import { Container } from 'react-bootstrap'
 import MyTable from '@/components/table'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Ex2Layout({
     children,
 }: {
@@ -14,6 +16,7 @@ export default function Ex2Layout({
                 {children}
             </Container>
             <Footer />
+            <ToastContainer />
         </>
 
     )
