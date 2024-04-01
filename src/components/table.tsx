1ed81b04ce69629f4a888a595c21de2c0f032b4a
lephuchung <lephuchung003@gmail.com>
2024-04-02 02:45:40 +0700

    video31

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,6 +5,8 @@ import MyModal from '@/components/modal'
 import { useState } from 'react';
 import EditModal from './EditModal';
 import Link from 'next/link';
+import { toast } from 'react-toastify';
+import { mutate } from 'swr';
 
 interface IProps {
     blogs: IBlog[]
@@ -22,9 +24,33 @@ function MyTable(props: IProps) {
         setShowEdit(true);
     };
 
+    const handleOnClickDelete = (id: number) => {
+
+        let text: string;
+        if (confirm(`Do you want to delete this blog (id = ${id})`) == true) {
+            text = 'ok';
+        } else text = 'no';
+
+        if (text === 'ok') {
+            fetch(`http://localhost:8000/blogs/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json()).then(res => {
+                if (res) {
+                    toast.success('Delete success!');
+                    mutate('http://localhost:8000/blogs')
+                }
+            });
+        };
+
+    };
+
     return (
         <div>
-            <div className='mb-1' style={{
+            <div className='pb-1 pt-3' style={{
                 display: 'flex',
                 justifyContent: 'space-between'
             }}>
@@ -52,7 +78,7 @@ function MyTable(props: IProps) {
                                 <td>
                                     <Link className='btn btn-primary' href={`/example2/blogs/${item.id}`}>Detail</Link>
                                     <Button style={{ margin: '0 5px' }} variant='warning' onClick={() => { handleOnClickEdit(); setBlogEdit(item) }}>Edit</Button>
-                                    <Button variant='danger'>Delete</Button>
+                                    <Button variant='danger' onClick={() => handleOnClickDelete(item.id)}>Delete</Button>
                                 </td>
                             </tr>
                         )
